04c6baba57782d7adda3bde4de887c43ef3c8d97
Undhad Kaushik <kpkushpatel8@gmail.com>
2023-04-18 08:34:22 +0530

    Initial Commit

---
+++ TabBarController/TabBarViewController.swift
@@ -0,0 +1,20 @@
+//
+//  TabBarViewController.swift
+//  TabBarController
+//
+//  Created by undhad kaushik on 13/01/23.
+//
+
+import UIKit
+
+class TabBarViewController: UITabBar {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
